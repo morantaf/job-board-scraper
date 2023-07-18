d4d14358eda21106b6ae65e92d78adaefcf4167a
Moranta Fall <fall_moranta@live.fr>
2023-07-18 16:45:01 +0200

    Scraping the list of job of the 1st page

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pdb
-import pprint
+import re
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common import exceptions
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 chrome = webdriver.Chrome()
 
-ACCEPTED_CITIES = ["amsterdam","utrecht","rotterdam"]
-BLACKLISTED_WORDS = ["senior", "lead", "php", "full-stack", "fullstack"]
+ACCEPTED_CITIES = {"amsterdam","utrecht","rotterdam", "hoofddorp"}
+BLACKLISTED_WORDS = {"senior", "lead", "php", "full-stack", "fullstack"}
+LANGUAGES_DESIRED = {"java", "python"}
 
 def has_blacklisted_word(title): 
     for word in title.lower().split():
@@ -16,32 +18,61 @@ def has_blacklisted_word(title):
             return True
     return False
 
+def has_city(location):
+    for word in location.lower().split():
+        if word in ACCEPTED_CITIES:
+            return True
+    return False
+
 def filter_div(div):
     title = div.find_element(By.CLASS_NAME, "jcs-JobTitle").text
     location = div.find_element(By.CLASS_NAME, "companyLocation").text
-    if not location.lower() in ACCEPTED_CITIES and has_blacklisted_word(title):
+    
+    if not has_city(location) or has_blacklisted_word(title):
         return False
     return True
 
-def main():
-    chrome.get("https://nl.indeed.com/jobs?q=Backend+Developer&l=Amsterdam&from=searchOnHP")
-    wait = WebDriverWait(chrome, timeout=20)
+def refers_language(requirement):
+    for word in requirement.lower().split():
+        re.sub("([\,\s\(\)\.\;])+","",word)
+        if word in LANGUAGES_DESIRED:
+            return True
+    return False
+
+# Used to slow down the execution to bypass the anti-bot protection
+def sleep(seconds):
+    time.sleep(seconds)
+    return True
+
+def accept_cookie(driver, wait):
     wait.until(lambda d: d.find_element(By.CLASS_NAME,"ot-sdk-container"))
-    cookie_button = chrome.find_element(By.ID, "onetrust-reject-all-handler")
+    cookie_button = driver.find_element(By.ID, "onetrust-reject-all-handler")
     if cookie_button:
         cookie_button.click()
+        sleep(3)
+
+
+def main():
+    chrome.get("https://nl.indeed.com/jobs?q=Backend+Developer&l=Amsterdam&fromage=7")
+    wait = WebDriverWait(chrome, timeout=20)
+    final_job_list = []
+
+    accept_cookie(chrome, wait)
+
     jobs = chrome.find_elements(By.CLASS_NAME, "resultContent")
     filtered_jobs = list(filter(filter_div,jobs))
-    job = filtered_jobs[0]
-    job.find_element(By.CLASS_NAME, "jcs-JobTitle").click()
-    pdb.set_trace()
-    description = wait.until(EC.presence_of_element_located((By.CLASS_NAME,"jobsearch-embeddedBody")))
-    # pprint.pprint(description.get_attribute("innerHTML"))
-    # pprint.pprint(list(map(lambda x: x.text, lists)))
-    # for job in filtered_jobs:
-    #     job.find_element(By.CLASS_NAME, "jcs-JobTitle").click()
-    #     wait.until(EC.presence_of_element_located((By.CLASS_NAME,"jobsearch-embeddedBody")))
+    for job in filtered_jobs[:4]:
+        job.find_element(By.CLASS_NAME, "jcs-JobTitle").click()
+        try:
+            description = wait.until(lambda c: c.find_element(By.CLASS_NAME,"jobsearch-JobComponent"))
+            job_requirements = description.find_elements(By.TAG_NAME,"li")
+            requirement_list = list(map(lambda x: x.text, job_requirements))
+            for requirement in requirement_list:
+                if refers_language(requirement):
+                    final_job_list.append(job)
+        except exceptions.TimeoutException:
+            print(f"didn't find description for {job.text}")
     
-    #print(links)
+        sleep(3)
 
 main()
